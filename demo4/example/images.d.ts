745527b2fa21380dbbd64488cdedaa3bd90c77b5
zhangji <zhangji@dfwsgroup.com>
2020-06-11 14:43:01 +0800

    feat: 删除无效代码

--- demo4/example/images.d.ts
+++
@@ -1,9 +0,0 @@
-declare module '*.svg'
-declare module '*.png'
-declare module '*.jpg'
-declare module 'react-highlight'
-declare module 'rc-queue-anim'
-declare module 'rc-texty'
-declare module 'rc-tween-one'
-
-
